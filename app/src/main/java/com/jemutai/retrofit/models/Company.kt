2e7da5231efb8eead85fad5409e89ad0d4e22952
Jemutai <janetjemutai19@gmail.com>
2024-05-15 15:03:37 +0300

    Models added

---
+++ app/src/main/java/com/jemutai/retrofit/models/Company.kt
@@ -0,0 +1,7 @@
+package com.jemutai.retrofit.models
+
+data class Company(
+    val  name:String,
+    val catchPhrase:String,
+    val  bs :String
+)
---
+++ app/src/main/java/com/jemutai/retrofit/models/User.kt
@@ -0,0 +1,10 @@
+package com.jemutai.retrofit.models
+
+data class User(
+    val id:Int,
+    val name:String,
+    val  username:String,
+    val email :String,
+    val  company:Company,
+    val website:String
+)
